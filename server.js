b3ed1bf35554682a1616575418ce1272e290c5ea
Jason <user@email.com>
2022-01-23 18:47:40 -0500

    imageissues

--- server.js
+++ server.js
@@ -5,27 +5,45 @@ const PORT = 3000;
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
-
-// db
-const products = require('./products');
+app.use((req, res, next) => {    
+	console.log(`${req.method} ${req.originalUrl}`);    
+	next();//test
+});
+// db in this case products folder
+const products = require('./models/products');
+//---set view engine---
+app.set ('view engine', 'ejs');
+// ----set static files---
+//---configure to look in public
+app.set (express.static('public'));
 
 // product index route
 app.get('/products', (req, res) => {
-  res.send(products);
-});
-
-// product show route
-app.get('/products/:id', (req, res) => {
-  res.send(products[req.params.id]);
+res.render('index.ejs', {products:products});
 });
 
 // product create route
-app.post('/products', (req, res) => {
-  console.log('CREATE route accessed');
+// ---gets the form---
+app.get('/products/new', (req, res) => {
+res.render('newForm.ejs');
+});
+// ---pushes the forms key value pairs into products---
+app.post('/products/', (req, res) => {
+  console.log('product created route accessed');
   console.log('Data within req.body: ', req.body);
   products.push(req.body);
-  res.redirect('/products');
+  res.redirects('/products')
+  //redirects to products which should include the addition
 });
+// product show route
+// ---shows one product at a time id reperesnts index number---
+app.get('/products/:id', (req, res) => {
+  const productsId = products[req.params.id];
+  res.render('show.ejs', {product: productsId});
+});
+// render the products object to each .ejs view.
+
+
 
 
 app.listen(PORT, () => {
